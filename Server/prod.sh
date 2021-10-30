bc3e5acff9f58611e9413e257e31b1a85c20e91f
deformal <drake9211@gmail.com>
2021-10-30 20:26:32 +0530

    prod-v-3

--- Server/prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo Adding pm2********************************
-yarn global add pm2 
-echo Moving to production**********************
-echo Building**********************************
-yarn build 
-echo Distibution*******************************
-yarn dist
-
